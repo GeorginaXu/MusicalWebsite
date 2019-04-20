be7bbb891cf6cad632288c37c08ade028015c7d1
GeorginaXu <cxu@smith.edu>
2019-04-20 13:22:40 -0400

    Add GET response and callback for quiz questions. Query data from database based on user's answers.

--- node.js
+++ node.js
@@ -26,6 +26,28 @@ var server = require ('http').createServer (app);
 app.use(express.static(__dirname));
 app.set('view engine', 'pug');
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
 var fs = require('fs');
 
 const sqlite3 = require('sqlite3').verbose();
@@ -36,19 +58,12 @@ let db = new sqlite3.Database('Musicals.db', sqlite3.OPEN_READWRITE, (err) => {
                                        console.log('Connected to the in-memory SQlite database.');
                                      });
 
-var r = "";
-db.all("SELECT Title FROM OnBroadway", function(err, rows) {
-        rows.forEach(function (row) {
-            r = r + row.Title;
-        })
-	});
-
-db.close((err) => {
+/*db.close((err) => {
   if (err) {
     return console.error(err.message);
   }
   console.log('Close the database connection.');
-});
+});*/
 
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname+'/website.html'));
@@ -58,20 +73,217 @@ app.get('/quiz.html',function(req,res){
   res.sendFile(path.join(__dirname+'/quiz.html'));
 });
 
+
+// Query and filter quiz result
+
 app.get('/result',function(req,res){
   res.render('result', {
-              Titles : r
+              Titles : quiz_result[0]
           });
 });
 
-app.post('/phantom', function(req, res){
-    var r = "";
-    db.all("SELECT * FROM OnBroadway WHERE Title like '%Phantom%'", function(err, rows) {
+
+app.get('/all', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals", function(err, rows) {
+        if(!err) {
             rows.forEach(function (row) {
-                res.render('result', {
-                Titles : row[0]})
+                if(result.indexOf(row.Title) == -1) {
+                    result.push(row.Title);
+                }
             });
-    	});
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+     });
+});
+
+app.get('/disney', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `Type of Musical` LIKE '%Disney%'", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                if(result.indexOf(row.Title) == -1) {
+                    result.push(row.Title);
+                }
+            });
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.get('/no_disney', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `Type of Musical` NOT LIKE '%Disney%';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                     result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
+});
+
+app.get('/romance', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `Type of Musical` LIKE '%Romance%';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                    result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
+});
+
+app.get('/comedy', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `Type of Musical` LIKE '%Comedy%';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                     result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
+});
+
+app.get('/crime', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `Type of Musical` LIKE '%Crime%';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                     result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
+});
+
+app.get('/history', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `Type of Musical` LIKE '%History%';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                    result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
+});
+
+app.get('/horror', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `Type of Musical` LIKE '%Horror%';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                    result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
+});
+
+app.get('/magic', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `Type of Musical` LIKE '%Magic%';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                     result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
+});
+
+app.get('/nyc', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `City` LIKE 'New York City';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                    result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
+});
+
+app.get('/other_cities', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `City` LIKE 'OffBroadway';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                    result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
+});
+
+app.get('/london', function(req, res){
+    let result = [];
+    db.all("SELECT Title FROM Musicals WHERE `City` LIKE 'London';", function(err, rows) {
+        if(!err) {
+            rows.forEach(function (row) {
+                 if(result.indexOf(row.Title) == -1) {
+                    result.push(row.Title);
+                 }
+            });
+            console.log(result);
+            res.send(result);
+        } else {
+            console.log(err);
+        }
+        });
 });
 
 
